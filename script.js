7dd70fedd2bad19db770d509a3e3d71d678a0c79
Alekh Srivastava <86128289+alekh9936@users.noreply.github.com>
2023-03-05 20:48:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  
+if((username ==="Admin"&&password ==="Admin")||
+(username ==="Test"&&password ==="Test")||
+(username ==="Student"&&password ==="Student")||
+(username ==="Faculty"&&password ==="Faculty")||
+(username ==="Bogus"&&password ==="Bogus")){
+  alert("Login successful, Redirecting!")
+  window.location.href = "homepage.html";
+}
+else if(username ===""||password ===""){
+  alert("Credentials required!")
+}
+else{
+  alert("Bad credentials!")
+}
+
+}
